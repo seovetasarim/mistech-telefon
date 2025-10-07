86e2054a836c96b1221620d3fb95cd3abc7f4b8d
iddaasohbet <iddaasohbet@users.noreply.github.com>
2025-10-07 14:55:26 +0300

    fix: update authOptions imports to '@/lib/auth'

--- src/app/api/admin/orders/route.ts
+++ src/app/api/admin/orders/route.ts
@@ -3,7 +3,7 @@ export const revalidate = 0;
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function GET(request: Request) {
   try {
